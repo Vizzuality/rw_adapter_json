3f3dc94905e84002e88e0aad49ac4160b968d587
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-05-03 10:39:33 +0200

    Replace typhoeus http client with curb

--- app/models/connector_service.rb
+++ app/models/connector_service.rb
@@ -1,32 +1,34 @@
-require 'typhoeus'
+require 'curb'
 require 'uri'
 require 'oj'
 
 class ConnectorService
   class << self
-    def establish_connection(url, method, params={})
-      @request = ::Typhoeus::Request.new(URI.escape(url), method: method, headers: { 'Accept' => 'application/json' }, params: params)
-      @request.run
-    end
-
     def connect_to_dataset_service(dataset_id, status)
       status   = case status
                  when 'saved' then 1
                  when 'deleted' then 3
                  else 2
                  end
+
       params   = { dataset: { dataset_attributes: { status: status } } }
       url      = URI.decode("#{ENV['API_DATASET_META_URL']}/#{dataset_id}")
 
-      establish_connection(url, 'put', params)
+      @c = Curl::Easy.http_put(URI.escape(url), Oj.dump(params)) do |curl|
+        curl.headers['Accept']       = 'application/json'
+        curl.headers['Content-Type'] = 'application/json'
+      end
     end
 
     def connect_to_provider(connector_url, data_path)
       url = URI.decode(connector_url)
 
-      establish_connection(url, 'get')
+      @c = Curl::Easy.http_get(URI.escape(url)) do |curl|
+        curl.headers['Accept']       = 'application/json'
+        curl.headers['Content-Type'] = 'application/json'
+      end
 
-      Oj.load(@request.response.body.force_encoding(Encoding::UTF_8))[data_path] || Oj.load(@request.response.body.force_encoding(Encoding::UTF_8))
+      Oj.load(@c.body_str.force_encoding(Encoding::UTF_8))[data_path] || Oj.load(@c.body_str.force_encoding(Encoding::UTF_8))
     end
   end
 end
