26cd09343ed197dfe6898b3f0284ed9bde6ee333
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-07-13 12:08:07 +0200

    remove worker

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,9 +2,9 @@ host = ENV.fetch('REDIS_PORT_6379_TCP_ADDR') { 'localhost' }
 port = ENV.fetch('REDIS_PORT_6379_TCP_PORT') { 6379 }
 
 Sidekiq.configure_server do |config|
-  config.redis = { size: 10, url: "redis://#{host}:#{port}/12", namespace: "RwAdapterJson_#{Rails.env}" }
+  config.redis = { url: "redis://#{host}:#{port}/12", namespace: "RwAdapterJson_#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { size: 1, url: "redis://#{host}:#{port}/12", namespace: "RwAdapterJson_#{Rails.env}" }
+  config.redis = { url: "redis://#{host}:#{port}/12", namespace: "RwAdapterJson_#{Rails.env}" }
 end
